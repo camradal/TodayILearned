b636fb8d6459c026b9c97d1e3a0d39fc8c5b4c0e
Alexey Kalinin <camradal@yahoo.com>
2012-10-07 11:37:11 -0700

    introduce a second project for common data

---
+++ TodayILearned.Core/ViewModels/ItemViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TodayILearned
+{
+    public class ItemViewModel : INotifyPropertyChanged
+    {
+        private string _title;
+        private string _description;
+        private string _url;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    NotifyPropertyChanged("Title");
+                }
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                if (value != _description)
+                {
+                    _description = value;
+                    NotifyPropertyChanged("Description");
+                }
+            }
+        }
+
+        public string Url
+        {
+            get
+            {
+                return _url;
+            }
+            set
+            {
+                if (value != _url)
+                {
+                    _url = value;
+                    NotifyPropertyChanged("Url");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ TodayILearned.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Net;
+
+
+namespace TodayILearned
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            this.Items = new ObservableCollection<ItemViewModel>();
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ItemViewModel> Items { get; private set; }
+
+        private string _sampleProperty = "Sample Runtime Property Value";
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string SampleProperty
+        {
+            get
+            {
+                return _sampleProperty;
+            }
+            set
+            {
+                if (value != _sampleProperty)
+                {
+                    _sampleProperty = value;
+                    NotifyPropertyChanged("SampleProperty");
+                }
+            }
+        }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public void LoadData()
+        {
+            var uri = new Uri("http://www.reddit.com/r/todayilearned.json");
+            var client = new WebClient();
+            client.DownloadStringAsync(uri);
+            client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(client_DownloadStringCompleted);
+            // Sample data; replace with real data
+            this.Items.Add(new ItemViewModel() { Title = "runtime one", Description = "Maecenas praesent accumsan bibendum", Url = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime two", Description = "Dictumst eleifend facilisi faucibus", Url = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime three", Description = "Habitant inceptos interdum lobortis", Url = "Habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime four", Description = "Nascetur pharetra placerat pulvinar", Url = "Ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime five", Description = "Maecenas praesent accumsan bibendum", Url = "Maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime six", Description = "Dictumst eleifend facilisi faucibus", Url = "Pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime seven", Description = "Habitant inceptos interdum lobortis", Url = "Accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime eight", Description = "Nascetur pharetra placerat pulvinar", Url = "Pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime nine", Description = "Maecenas praesent accumsan bibendum", Url = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime ten", Description = "Dictumst eleifend facilisi faucibus", Url = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime eleven", Description = "Habitant inceptos interdum lobortis", Url = "Habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime twelve", Description = "Nascetur pharetra placerat pulvinar", Url = "Ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime thirteen", Description = "Maecenas praesent accumsan bibendum", Url = "Maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime fourteen", Description = "Dictumst eleifend facilisi faucibus", Url = "Pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime fifteen", Description = "Habitant inceptos interdum lobortis", Url = "Accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat" });
+            this.Items.Add(new ItemViewModel() { Title = "runtime sixteen", Description = "Nascetur pharetra placerat pulvinar", Url = "Pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum" });
+
+            this.IsDataLoaded = true;
+        }
+
+        void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
