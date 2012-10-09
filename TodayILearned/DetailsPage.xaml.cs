87bb27778bb7026155278e298fd0dfb3261f6455
Alexey Kalinin <camradal@yahoo.com>
2012-10-08 21:03:11 -0700

    update with details page and ads

---
+++ TodayILearned/DetailsPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Navigation;
+using TodayILearned.Resources;
+using Utilities;
+
+namespace TodayILearned
+{
+    public partial class DetailsPage
+    {
+        private volatile bool navigating;
+        private const string sourceUriFormat = @"http://en.wikipedia.org{0}";
+        private Uri sourceUrl;
+
+        public DetailsPage()
+        {
+            InitializeComponent();
+
+            webBrowser1.Navigated += webBrowser1_Navigated;
+            webBrowser1.LoadCompleted += webBrowser1_LoadCompleted;
+
+            // ads
+            AdBox.ErrorOccurred += AdBox_ErrorOccurred;
+            AdBox.AdRefreshed += AdBox_AdRefreshed;
+        }
+
+        #region Navigation
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            string uri;
+            if (NavigationContext.QueryString.TryGetValue("uri", out uri))
+            {
+                string decodedUri = HttpUtility.HtmlDecode(uri);
+                string sourceString = string.Format(sourceUriFormat, decodedUri);
+                sourceUrl = new Uri(sourceString, UriKind.Absolute);
+                webBrowser1.Source = sourceUrl;
+            }
+            //else if (isNewPage && State.ContainsKey("SourceUrl"))
+            //{
+            //    sourceUrl = new Uri(State["SourceUrl"].ToString());
+            //    webBrowser1.Source = sourceUrl;
+            //}
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            if (navigating)
+            {
+                GlobalLoading.Instance.IsLoading = false;
+                GlobalLoading.Instance.LoadingText = null;
+                navigating = false;
+            }
+
+            if (e.NavigationMode != NavigationMode.Back)
+            {
+                //State["SourceUrl"] = sourceUrl;
+            }
+        }
+
+        #endregion
+
+        #region Web browser
+
+        void webBrowser1_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (!navigating)
+            {
+                GlobalLoading.Instance.IsLoading = true;
+                GlobalLoading.Instance.LoadingText = Strings.Loading;
+            }
+            navigating = true;
+        }
+
+        private void webBrowser1_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            if (navigating)
+            {
+                GlobalLoading.Instance.IsLoading = false;
+                GlobalLoading.Instance.LoadingText = null;
+            }
+            navigating = false;
+        }
+
+        #endregion
+
+        #region Ads
+
+        void AdBox_AdRefreshed(object sender, EventArgs e)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                AdDuplexAdControl.Visibility = Visibility.Collapsed;
+                AdBox.Visibility = Visibility.Visible;
+            });
+        }
+
+        void AdBox_ErrorOccurred(object sender, Microsoft.Advertising.AdErrorEventArgs e)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                AdBox.Visibility = Visibility.Collapsed;
+                AdDuplexAdControl.Visibility = Visibility.Visible;
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ TodayILearned/GlobalLoading.cs
@@ -0,0 +1,130 @@
+﻿using System.ComponentModel;
+using System.Windows.Navigation;
+//using AgFx;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Utilities
+{
+    public class GlobalLoading : INotifyPropertyChanged
+    {
+        private ProgressIndicator indicator;
+        private static GlobalLoading instance;
+        private int loadingCount;
+        private string text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsDataManagerLoading { get; set; }
+
+        public bool ActualIsLoading
+        {
+            get
+            {
+                return IsLoading || IsDataManagerLoading;
+            }
+        }
+
+        public bool IsLoading
+        {
+            get
+            {
+                return loadingCount > 0;
+            }
+            set
+            {
+                bool loading = IsLoading;
+                if (value)
+                {
+                    ++loadingCount;
+                }
+                else
+                {
+                    --loadingCount;
+                }
+
+                NotifyValueChanged();
+            }
+        }
+
+        public string LoadingText
+        {
+            set
+            {
+                text = value;
+                NotifyValueChanged();
+            }
+        }
+
+        public static GlobalLoading Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GlobalLoading();
+                }
+
+                return instance;
+            }
+        }
+
+        private GlobalLoading()
+        {
+        }
+
+        public void Initialize(PhoneApplicationFrame frame)
+        {
+            //DataManager.Current.PropertyChanged += OnDataManagerPropertyChanged;
+
+            indicator = new ProgressIndicator();
+            frame.Navigated += OnRootFrameNavigated;
+        }
+
+        private void OnRootFrameNavigated(object sender, NavigationEventArgs e)
+        {
+            // Use in Mango to share a single progress indicator instance.
+            var ee = e.Content;
+            var pp = ee as PhoneApplicationPage;
+            if (pp != null)
+            {
+                pp.SetValue(SystemTray.ProgressIndicatorProperty, indicator);
+            }
+        }
+
+        private void OnDataManagerPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if ("IsLoading" == e.PropertyName)
+            {
+                //IsDataManagerLoading = DataManager.Current.IsLoading;
+                NotifyValueChanged();
+            }
+        }
+
+        private void NotifyValueChanged()
+        {
+            if (indicator != null)
+            {
+                indicator.IsIndeterminate = loadingCount > 0 || IsDataManagerLoading;
+
+                // set text value
+                indicator.Text = text;
+
+                // for now, just make sure it's always visible.
+                if (indicator.IsVisible == false)
+                {
+                    indicator.IsVisible = true;
+                }
+            }
+        }
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
