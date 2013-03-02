cc7b22b3433337f872f837b3339224c4f025be07
Alexey Kalinin <camradal@yahoo.com>
2013-03-02 10:59:21 -0800

    Fix about page

--- TodayILearned/AboutPage.xaml.cs
+++ TodayILearned/AboutPage.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Windows;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Tasks;
@@ -75,51 +73,7 @@ namespace TodayILearned
 
         private void ReadVersionFromManifest()
         {
-            Uri manifest = new Uri("WMAppManifest.xml", UriKind.Relative);
-            var si = Application.GetResourceStream(manifest);
-            if (si != null)
-            {
-                using (StreamReader sr = new StreamReader(si.Stream))
-                {
-                    bool haveApp = false;
-                    while (!sr.EndOfStream)
-                    {
-                        string line = sr.ReadLine();
-                        if (!haveApp)
-                        {
-                            int i = line.IndexOf("AppPlatformVersion=\"", StringComparison.InvariantCulture);
-                            if (i >= 0)
-                            {
-                                haveApp = true;
-                                line = line.Substring(i + 20);
-
-                                int z = line.IndexOf("\"");
-                                if (z >= 0)
-                                {
-                                    // if you're interested in the app plat version at all
-                                    // AppPlatformVersion = line.Substring(0, z);
-                                }
-                            }
-                        }
-
-                        int y = line.IndexOf("Version=\"", StringComparison.InvariantCulture);
-                        if (y >= 0)
-                        {
-                            int z = line.IndexOf("\"", y + 9, StringComparison.InvariantCulture);
-                            if (z >= 0)
-                            {
-                                // We have the version, no need to read on.
-                                versionText.Text = line.Substring(y + 9, z - y - 9);
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                versionText.Text = "Unknown";
-            }
+            versionText.Text = "1.0";
         }
     }
 }
