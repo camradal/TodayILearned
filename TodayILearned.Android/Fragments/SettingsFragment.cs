d5e9b61bea573315e532610ba1ab347317a10fd7
Giorgi <mcnamaragio@gmail.com>
2013-09-26 23:40:02 +0400

    Reformatting code

--- TodayILearned.Android/Fragments/SettingsFragment.cs
+++ TodayILearned.Android/Fragments/SettingsFragment.cs
@@ -1,14 +1,6 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Preferences;
-using Android.Text;
-using Android.Views;
-using Android.Widget;
 
 namespace TodayILearned.AndroidApp
 {
