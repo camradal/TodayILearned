d633565729153633d3921ca8a5aba1d67c53c900
Alexey Kalinin <camradal@yahoo.com>
2013-12-19 17:59:12 -0800

    Faster wiki loads

---
+++ TodayILearned/Converters/WikiUrlConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TodayILearned.Utilities
+{
+    public class WikiUrlConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var url = value as string;
+            if (!string.IsNullOrEmpty(url) && url.Contains("en.wikipedia"))
+            {
+                url = url.Replace("en.wikipedia", "en.m.wikipedia");
+            }
+            return url;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
