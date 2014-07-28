80d59d14ecc03dc1d5564689498a5fe72a2df1c9
Alexey Kalinin <camradal@yahoo.com>
2014-07-27 21:44:50 -0700

    Bump up the version

--- TodayILearned/AboutPage.xaml.cs
+++ TodayILearned/AboutPage.xaml.cs
@@ -27,7 +27,7 @@ namespace TodayILearned
                     },
                     new NewItem
                     {
-                        Version = "1.8.4",
+                        Version = "1.8.5",
                         Description =
                             "- Bug fixes"
                     },
@@ -143,7 +143,7 @@ namespace TodayILearned
 
         private void ReadVersionFromManifest()
         {
-            versionText.Text = "1.8.4";
+            versionText.Text = "1.8.5";
         }
     }
 }
